2f6587d1024c9f4f191cd16e3e26655920e10a59
Charlie Bi <charlie_bi@hotmail.com>
2023-04-21 07:36:05 -0400

    fixed bugs with alert

--- react-frontend/src/pages/AlertCard.jsx
+++ react-frontend/src/pages/AlertCard.jsx
@@ -2,14 +2,14 @@
 export default function AlertCard({alert}) {
   return (
     <div className="col-md-4">
-        <div className="card" mb-3>
+        <div className="card">
             <div className="card-body">
                 <div className="d-flex justify-content-between align-items-center">
-                    <h5 className="card-title">{alert.name}</h5>
-                    <a className="btn btn-light" href={`/alerts/${alert.id}`}>View</a>
+                    <h5 className="card-title">{alert.alertName}</h5>
+                    <a className="btn btn-light" href={`/alerts/${alert._id}`}>View</a>
 
                 </div>
-                <p className="card-text">{alert.description}</p>
+                <p className="card-text">{alert.alertDescription}</p>
                 <p className="small">Status: <strong>{alert.status}</strong></p>
 
             </div>
--- react-frontend/src/pages/Alerts.jsx
+++ react-frontend/src/pages/Alerts.jsx
@@ -1,18 +1,52 @@
-import { useQuery } from "@apollo/client";
-import { GET_ALERTS } from "../queries/alertQueries";
+import { useQuery, gql } from "@apollo/client";
+import {useState, useEffect} from "react";
+//import { GET_ALERTS } from "../queries/alertQueries";
 import AlertCard from "./AlertCard";
 
+//putting this here because I was getting an error with the import statement
+const GET_ALERTS = gql`
+{
+    getAlerts {
+        _id
+        alertName
+        alertDescription
+        status 
+    }
+}
+`;
+
+
 export default function Alerts() {
+
+    const [alertList, setAlertList] = useState([]);
+    const [message, setMessage] = useState("");
+
+    
+
     const { loading, error, data } = useQuery(GET_ALERTS);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+
+    useEffect(()=>{
+        if(loading){
+            setMessage("loading...")
+
+        }
+        else if(data){
+            setAlertList(data.getAlerts)
+            console.log(data.getAlerts)
+        }else if(error){
+            setMessage("Error:")
+        }      
+
+    }, [loading,data, error]);
+
 
   return (
     <>
-        {data.alerts.length > 0 ? (
+    <p>{message}</p>
+        {alertList.length > 0 ? (
             <div className="row mt-4">
-                {data.alerts.map((alert) => (
-                    <AlertCard key={alert.id} alert={alert} />
+                {alertList.map((item) => (
+                    <AlertCard key={item._id} alert={item} />
                 ))}
             </div>
         ) : (
